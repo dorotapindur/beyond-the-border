d6a593dcde5f1d07927992217a1c5404dc9ca4ff
Dorota Pindur <dpindur@wp.pl>
2021-03-01 09:18:41 +0100

    add gradient type appearing options

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
+
+
+//choose linear/radial - options appearing
+const linearType = document.querySelector('.type-linear--js');
+const radialType = document.querySelector('.type-radial--js');
+const linearBox = document.querySelector('.linear');
+const radialBox = document.querySelector('.radial');
+
+linearType.addEventListener('change', () => {
+    if (linearType.value === 'on') {
+        linearBox.classList.remove('invisible');
+        radialBox.classList.add('invisible');
+    }
+});
+radialType.addEventListener('change', () => {
+    if (radialType.value === 'on') {
+        radialBox.classList.remove('invisible');
+        linearBox.classList.add('invisible');
+    }
+});
+
+//choose angle - handle moving
 const chooseAngle = document.querySelector('.choose-angle--js');
 let angleText = document.querySelector('.angle-handle__text');
 const indicator = document.querySelector('.angle-handle__indicator');
-console.log(indicator);
-
 angleText.innerHTML = `${chooseAngle.value}&deg;`;
 
-chooseAngle.addEventListener('input', updateValue)
+chooseAngle.addEventListener('input', updateValue);
 function updateValue(event) {
     let angle = event.target.value;
     angleText.innerHTML = `${angle}&deg;`;
     indicator.style.transform = `rotate(${angle}deg)`;
-}
-
-
-
-
-
-
-const inputList = document.querySelectorAll('.beginning__input');
-const valueText = document.querySelector('.beginning__value--js');
+};
+//choose beginning
+const inputList = document.querySelectorAll('.radial__input');
+const valueText = document.querySelector('.radial__value--js');
 
 valueText.textContent = 'center';
 function getValue() {
