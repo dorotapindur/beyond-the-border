6abaf01da9e0015bb9c6b0b712277714e435afe8
Dorota Pindur <dpindur@wp.pl>
2021-03-03 20:55:30 +0100

    create getRadialOptions module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { width, getWidth } from './js_modules/getWidth.js';
 import { angle, getAngle } from './js_modules/getAngle.js';
+import { circle, ellipse, radialBeginning, getRadialShape, getBeginningValue } from './js_modules/getRadialOptions.js';
 
 const outputCode = document.getElementById('output-code');
 
@@ -11,14 +12,6 @@ const secondColor = document.querySelector('.second-color');
 
 
 getWidth();
-getAngle();
-
-const seeCode = document.getElementById('see-code');
-seeCode.addEventListener('click', (event) => {
-    event.preventDefault();
-    outputCode.innerHTML = `My width is ${width}. My angle is ${angle}.`;
-})
-
 
 //choose linear/radial - options appearing
 const linearType = document.querySelector('.type-linear--js');
@@ -30,30 +23,27 @@ linearType.addEventListener('change', () => {
     if (linearType.value === 'on') {
         linearBox.classList.remove('invisible');
         radialBox.classList.add('invisible');
+        getAngle();
     }
 });
 radialType.addEventListener('change', () => {
     if (radialType.value === 'on') {
         radialBox.classList.remove('invisible');
         linearBox.classList.add('invisible');
+        getRadialShape();
+        getBeginningValue();
     }
 });
 
+const seeCode = document.getElementById('see-code');
+seeCode.addEventListener('click', (event) => {
+    event.preventDefault();
+    outputCode.innerHTML = `My width is ${width}. My angle is ${angle}. Radial shape: circle - ${circle}, ellipse - ${ellipse}. Radial beginning is: ${radialBeginning}`;
+})
 
-//choose beginning
-const inputList = document.querySelectorAll('.radial__input');
-const valueText = document.querySelector('.radial__value--js');
 
-valueText.textContent = 'center';
-function getValue() {
-    for (let i = 0; i< inputList.length; i++) {
-        inputList[i].addEventListener('click', () => {
-            valueText.textContent = inputList[i].value;
-        });
-    }
-}
-getValue();
 
+//aside - about "this"
 function greet() {
     const reply = `Hello ${this.name} ${this.surname}!`;
     console.log(reply);
---
+++ js_modules/getRadialOptions.js
@@ -0,0 +1,42 @@
+
+let circle = true;
+let ellipse = false;
+
+function getRadialShape() {
+    const circleInput = document.querySelector('.radial__circle-input');
+    const ellipseInput = document.querySelector('.radial__ellipse-input');
+    circleInput.addEventListener('change', () => {
+        if (circleInput.value === 'on') {
+            circle = true;
+            ellipse = false
+        } else {
+            circle = false;
+            ellipse = true;
+        }
+    });
+    ellipseInput.addEventListener('change', () => {
+        if (ellipseInput.value === 'on') {
+            ellipse = true;
+            circle = false;
+        } else {
+            ellipse = false;
+            circle = true;
+        }
+    });
+}
+
+let radialBeginning;
+
+function getBeginningValue() {
+    const inputList = document.querySelectorAll('.radial__input');
+    const valueText = document.querySelector('.radial__value--js');
+    valueText.textContent = 'center';
+    for (let i = 0; i< inputList.length; i++) {
+        inputList[i].addEventListener('click', () => {
+            valueText.textContent = inputList[i].value;
+            radialBeginning = inputList[i].value;
+        });
+    }
+};
+
+export { circle, ellipse, radialBeginning, getRadialShape, getBeginningValue };
