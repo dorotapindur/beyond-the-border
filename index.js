d66e72c423d9227c2e2bc57680a5dc94f63f03b6
Dorota Pindur <dpindur@wp.pl>
2021-03-07 22:43:55 +0100

    add getGradientType module

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { width, height, getDimensions } from './js_modules/getDimensions.js';
-import { angle, getAngle } from './js_modules/getAngle.js';
-import { circle, ellipse, radialBeginning, getRadialShape, getBeginningValue } from './js_modules/getRadialOptions.js';
+import { angle } from './js_modules/getAngle.js';
+import { circle, ellipse, radialBeginning } from './js_modules/getRadialOptions.js';
 import { userBorderWidth, getBorderWidth } from './js_modules/getBorderWidth.js';
 import { userBorderRadius, borderRadiusUnit, getBorderRadius } from './js_modules/getBorderRadius.js';
 import { firstColor, secondColor, getColors } from './js_modules/getColors.js';
+import { gradientType, getGradientType } from './js_modules/getGradientType.js';
 const outputShape = document.getElementById('output-shape');
 
 export {outputShape};
@@ -12,34 +13,20 @@ getDimensions();
 getBorderWidth();
 getBorderRadius();
 getColors();
-
-//choose linear/radial - options appearing
-const linearType = document.querySelector('.type-linear--js');
-const radialType = document.querySelector('.type-radial--js');
-const linearBox = document.querySelector('.linear');
-const radialBox = document.querySelector('.radial');
+getGradientType();
 
 
-linearType.addEventListener('change', () => {
-    if (linearType.value === 'on') {
-        linearBox.classList.remove('invisible');
-        radialBox.classList.add('invisible');
-        getAngle();
-    }
-});
-radialType.addEventListener('change', () => {
-    if (radialType.value === 'on') {
-        radialBox.classList.remove('invisible');
-        linearBox.classList.add('invisible');
-        getRadialShape();
-        getBeginningValue();
-    }
-});
 
 const seeCode = document.getElementById('see-code');
 seeCode.addEventListener('click', (event) => {
     const outputCode = document.getElementById('output-code');
     event.preventDefault();
-    outputCode.innerHTML = `Dimensions: ${width}px x ${height}px. Border width equals ${userBorderWidth}px, border-radius is ${userBorderRadius}${borderRadiusUnit}. My angle is ${angle}deg. Radial shape: circle - ${circle}, ellipse - ${ellipse}. Radial beginning is: ${radialBeginning}. Colors: 1 = <span style="color: ${firstColor}">${firstColor}</span>, 2 = <span style="color: ${secondColor}">${secondColor}</span>`;
+    let gradientTypeContent;
+    if (gradientType === 'linear') {
+        gradientTypeContent = `My angle is ${angle}deg.`;
+    } else {
+        gradientTypeContent = `Radial shape: circle - ${circle}, ellipse - ${ellipse}. Radial beginning is: ${radialBeginning}.`
+    }
+    outputCode.innerHTML = `Dimensions: ${width}px x ${height}px. Border width equals ${userBorderWidth}px, border-radius is ${userBorderRadius}${borderRadiusUnit}. Gradient type: ${gradientType}. ${gradientTypeContent} Colors: 1 = <span style="color: ${firstColor}">${firstColor}</span>, 2 = <span style="color: ${secondColor}">${secondColor}</span>`;
 })
 
---
+++ js_modules/getGradientType.js
@@ -0,0 +1,31 @@
+import { getAngle } from "./getAngle.js";
+import { getBeginningValue, getRadialShape } from "./getRadialOptions.js";
+
+let gradientType = 'linear';
+
+function getGradientType() {
+    const linearType = document.querySelector('.type-linear--js');
+    const radialType = document.querySelector('.type-radial--js');
+    const linearBox = document.querySelector('.linear');
+    const radialBox = document.querySelector('.radial');
+
+    linearType.addEventListener('change', () => {
+        if (linearType.value === 'on') {
+            linearBox.classList.remove('invisible');
+            radialBox.classList.add('invisible');
+            getAngle();
+            gradientType = 'linear';
+        }
+    });
+    radialType.addEventListener('change', () => {
+        if (radialType.value === 'on') {
+            radialBox.classList.remove('invisible');
+            linearBox.classList.add('invisible');
+            getRadialShape();
+            getBeginningValue();
+            gradientType = 'radial';
+        }
+    });
+}
+
+export { gradientType, getGradientType };
