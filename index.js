ac98124e70f89ae62d58654daa10bbb1388d3f1c
Dorota Pindur <dpindur@wp.pl>
2021-03-09 13:35:18 +0100

    move canvas to js, enlarge and hide it

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ const outputShape = document.getElementById('output-shape');
 export {outputShape};
 
 const image = document.getElementById('output');
-const canvas = document.getElementById("canvas");
+const canvasContainer = document.querySelector('.canvas-container');
+const canvasSize = 3000;
+canvasContainer.innerHTML = `<canvas id="canvas" width="${canvasSize}px" height="${canvasSize}px" style="background-color: #000000"></canvas>`;
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const canvasSize = 600;
 let canvasImageWidth;
 let canvasImageHeight;
 
@@ -27,30 +29,31 @@ function drawImageOnCanvas() {
 const loadFile = function(event) {
 	image.src = URL.createObjectURL(event.target.files[0]);
     image.addEventListener('load', function() {
-        canvasImageWidth = image.width * 2;
-        canvasImageHeight = image.height * 2;
+        canvasImageWidth = image.naturalWidth;
+        canvasImageHeight = image.naturalHeight;
         drawImageOnCanvas();
     }) 
 };
 document.getElementById('photo').addEventListener('input', loadFile);
 const transformXinput = document.getElementById('transform-x');
 const transformYinput = document.getElementById('transform-y');
+const newImageDimensions = document.querySelector('.new-dimensions');
 
 function setDegrees() {
     const transX = transformXinput.value;
     const transY = transformYinput.value;
     let imageWidth = Math.floor(image.width * transX);
     let imageHeight = Math.floor(image.height * transY);
-    canvasImageWidth = imageWidth * 2;
-    canvasImageHeight = imageHeight * 2;
-    console.log(imageWidth + ' ' + imageHeight);
+    canvasImageWidth = Math.floor(image.naturalWidth * transX);
+    canvasImageHeight = Math.floor(image.naturalHeight * transY);
+    newImageDimensions.innerHTML = `new image dimensions: ${canvasImageWidth} x ${canvasImageHeight}`;
     document.querySelector('.transform-x-value').innerHTML = transX;
     document.querySelector('.transform-y-value').innerHTML = transY;
     image.style.transform = `scaleX(${transX}) scaleY(${transY})`;
     drawImageOnCanvas();
 }
-transformXinput.addEventListener('change', setDegrees);
-transformYinput.addEventListener('change', setDegrees);
+transformXinput.addEventListener('input', setDegrees);
+transformYinput.addEventListener('input', setDegrees);
 
 function downloadImg() {
     const downloadedImage = canvas.toDataURL("image/jpg");
